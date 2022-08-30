f8f3b284cf34867ef40eed7cb840d868ce8ecd67
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-30 17:30:47 -0400

    passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 
 function App() {
-  const [dogImage, setDogImage] = useState([]);
+  const [dogImage, setDogImage] = useState(null);
 
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random")
