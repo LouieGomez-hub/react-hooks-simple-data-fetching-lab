6a1cb13cec1b6152542351f48859232d7e241b2d
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-30 17:18:48 -0400

    app component and useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+  const [randomDog, setrandomDog] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then((data) => {
+        
+        setIsLoaded(true);
+    });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>
+}
+
+export default App;
