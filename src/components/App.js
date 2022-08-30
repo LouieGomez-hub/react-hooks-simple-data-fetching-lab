1d926f332a92bb8bbf477303e00dc393819fa8e6
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-30 17:22:12 -0400

    updated fetch

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
     .then((data) => {
-        
+      setDogImage(data.message)
     });
   }, []);
 
